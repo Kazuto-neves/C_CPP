8f9a9d884e6248a3f8b683fa4ffe77c8643c8a15
Christian <64045767+Kazuto-neves@users.noreply.github.com>
2022-03-26 18:07:26 -0300

    Update Casa.cpp

--- Glut/Treino/Casa.cpp
+++ Glut/Treino/Casa.cpp
@@ -7,10 +7,11 @@
 
 using namespace std;
 
-#define PI 3.1415926535
+#define M_PI       3.14159265358979323846
 
 float x = 0.300f;
 float X = 0.180f;
+float r = -0.80f;
 
 static void teclaPressionado(unsigned char tecla,int posicaoX,int posicaoY)
 {
@@ -19,16 +20,45 @@ case 'd':
 case 'D':
     if(x<=1.76f || x>=0.26f){
         x +=0.010f;
+        r +=0.010f;
     }
     //x +=0.010f;
     break;
 case 'a':
 case 'A':
     if(x<=1.76f || x>=0.26f){
-        x-=0.010f;
+        x -=0.010f;
+        r -=0.010f;
     }
     }
     glutPostRedisplay();
+}
+
+void circulo(float sx,float sy,float tx,float ty,float r,float g,float b)
+{
+    glPushMatrix();
+    glLoadIdentity();
+    glTranslatef(tx,ty,0.0f);
+    glScalef(sx,sy,0.0f);
+    //glTranslatef(0.70f, 0.50f, 0.0f);
+    //glScalef(0.15, 0.30f, 0.0f);
+    double ang1 = 2*  M_PI/100 ;
+    glPolygonMode( GL_FRONT, GL_FILL );
+    glColor3f(r,g,b);
+    glBegin(GL_POLYGON);
+        double ang2=0.0;
+        glVertex2d( cos(0.0) , sin(0.0));
+        int i;
+        for ( i=0 ; i< 100 ;i++)
+        {
+
+            glVertex2d( cos(ang2),sin(ang2));
+            ang2 += ang1 ;
+        }
+
+    glEnd();
+    glPopMatrix();
+    glFlush();
 }
 
 void inicia()
@@ -201,22 +231,7 @@ void tronco()
 
 void arvore()
 {
-    glPushMatrix();
-    glLoadIdentity();
-    glTranslatef(0.650f, 0.40f, 0.0f);
-    glRotatef(300.0,160.0,1.0,0.0);
-    glScalef(1.0,1.0,1.0);
-    glBegin(GL_TRIANGLES);
-
-    glColor3f(0.05,0.35,0);
-    glVertex2f(-0.3,-0.3);
-    glColor3f(0.08,0.57,0);
-    glVertex2f(0.3,-0.3);
-    glColor3f(0.09,0.7,0);
-    glVertex2f(0.0,0.9);
-    glPopMatrix();
-    glEnd();
-    glFlush();
+    circulo(0.25,0.30,0.650f,0.40f,0.05,0.35,0);
 }
 
 void Rua(){
@@ -310,27 +325,21 @@ void LanternaT(float x){
 }
 
 void rodas(float x){
-    glPushMatrix();
-    glLoadIdentity();
-    glTranslatef(x, -0.60f, 0.0f);
-    glColor4ub(20,20,20,255);
-    glBegin(GL_QUADS);
-    glVertex3f(-1.2f, 0.0f, 0.0f);                          // Top Left
-    glVertex3f(-1.09f, 0.0f, 0.0f);                          // Top Right
-    glVertex3f(-1.09f,-0.1f, 0.0f);                          // Bottom Right
-    glVertex3f(-1.2f,-0.1f, 0.0f);
-    glEnd();
-    glPopMatrix();
-    glFlush();
+    circulo(0.05,0.10,x,-0.65f,0.08, 0.08, 0.08);
 }
 
-void carro(float x){
+void moon(){
+    circulo(0.12,0.17,0.20f,0.70f,1, 0.94, 0.26);
+    circulo(0.12,0.17,0.25f,0.70f,0.09,0.11,0.25);
+}
+
+void carro(float x , float r){
     carroceria(x);
     frenteTras(x);
     LanternaF(x);
     LanternaT(x);
-    rodas(x+0.05);
-    rodas(x+0.25);
+    rodas(r);
+    rodas(r+0.20);
 }
 
 void faixas (float x){
@@ -356,7 +365,8 @@ void desenha()
     arvore();
     Rua();
     faixas(X);
-    carro(x);
+    carro(x,r);
+    moon();
 
 }
 
